1d7d2767911143eda646de66fcc3ed34bcf511d4
Andres Restrepo <86010642+1stprojectalfa@users.noreply.github.com>
2021-06-19 16:50:28 -0600

    Update volatility.py

--- volatility.py
+++ volatility.py
@@ -24,22 +24,3 @@ def plotting_scatter(x_axis):
 def plotting_histogram_distribution(x_axis):
     plt.hist(x_axis, color = 'r')
     plt.show()
-
-files = ['AAPL', 'AMD', 'BAC', 'EEM', 'GLD', 'IBM', 'MU', 'NIO', 'SBUX', 'SLV', 'SNAP', 'SPY', 'TLT', 'UBER', 'XOM', 'FXI']
-
-for name in files:
-    csv_file = name + '_Week_260.csv'
-    print('---------------------------------------------------------------------------------------------------')
-    print(csv_file)
-    data = read_data(csv_file)
-    data, abs_mean, abs_std = building_frame(data)
-    print(data)
-    print(f'Absolute Mean: {abs_mean}')
-    returns = data['Distance From Absolute Mean']
-    target = probabilities.building_target_range(returns, 0, abs_mean)
-    probabilities.printing_probabilities(returns, target)
-    values = list(data['Distance From Absolute Mean'])
-    runs = sequences.builiding_sequences(values, 0, np.amax(values))
-    sequences.display_sequences(runs)
-    periods_duration = sequences.building_lengths_distribution(runs)
-    print(periods_duration)
